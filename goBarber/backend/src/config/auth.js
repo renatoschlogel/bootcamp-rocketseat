ea4990709b689c5012e0c12e907668c387b95cc9
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-02-19 07:16:43 -0300

    goBarber transferido para o repositorio https://github.com/renatoschlogel/GoBarber-backend

--- goBarber/backend/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '56753623bfedfdeff1a602ed6083fe2f',
-  expiresIn: '7d'
-}
--- goBarber/backend/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-
-const port = 3333;
-app.listen(port);
-
-console.log(`Disponível na porta ${port}`);
